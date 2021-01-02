bfb779e41a3b008ae66dfe5c4eeb5d273bc72a28
dodotz613 <56112643+dodotz613@users.noreply.github.com>
2021-01-02 08:51:11 +0700

    Add files via upload

---
+++ daftar.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once("config.php");
+
+if(isset($_POST['daftar'])){
+
+    // filter data yang diinputkan
+    $nama_user = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    // enkripsi password
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $email_user = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+
+    // menyiapkan query
+    $sql = "INSERT INTO user (nama_user, email_user, username, password)
+            VALUES (:nama_user, :email_user, :username, :password)";
+    $stmt = $db->prepare($sql);
+
+    // bind parameter ke query
+    $params = array(
+        ":nama_user" => $nama_user,
+        ":email_user" => $email_user,
+        ":username" => $username,
+        ":password" => $password
+    );
+
+    // eksekusi query untuk menyimpan ke database
+    $saved = $stmt->execute($params);
+
+    // jika query simpan berhasil, maka user sudah terdaftar
+    // maka alihkan ke halaman login
+    if($saved) header("Location: login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+
+        <p>&larr; <a href="main.php">Home</a>
+
+        <h4>Bergabunglah bersama ribuan orang lainnya...</h4>
+        <p>Sudah punya akun? <a href="login.php">Login di sini</a></p>
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="name">Nama Lengkap</label>
+                <input class="form-control" type="text" name="name" placeholder="Nama kamu" />
+            </div>
+
+            <div class="form-group">
+                <label for="username">Email</label>
+                <input class="form-control" type="email" name="email" placeholder="Alamat Email" />
+            </div>
+
+            <div class="form-group">
+                <label for="email">Username</label>
+                <input class="form-control" type="Username" name="Username" placeholder="Username" />
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" placeholder="Password" />
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="daftar" value="Daftar" />
+
+        </form>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
