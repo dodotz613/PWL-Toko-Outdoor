8da2908de72db22410565b8c3d1ca314e43bfc18
dodotz613 <56112643+dodotz613@users.noreply.github.com>
2021-01-06 14:53:46 +0700

    Add File Cart

---
+++ cart.php
@@ -0,0 +1,122 @@
+<?php
+
+session_start();
+$status="";
+if (isset($_POST['action']) && $_POST['action']=="remove"){
+if(!empty($_SESSION["shopping_cart"])) {
+	foreach($_SESSION["shopping_cart"] as $key => $value) {
+		if($_POST["code"] == $key){
+		unset($_SESSION["shopping_cart"][$key]);
+		$status = "<div class='box' style='color:red;'>
+		Product is removed from your cart!</div>";
+		}
+		if(empty($_SESSION["shopping_cart"]))
+		unset($_SESSION["shopping_cart"]);
+			}
+		}
+}
+
+if (isset($_POST['action']) && $_POST['action']=="change"){
+  foreach($_SESSION["shopping_cart"] as &$value){
+    if($value['code'] === $_POST["code"]){
+        $value['quantity'] = $_POST["quantity"];
+        break; // Stop the loop after we've found the product
+    }
+}
+
+}
+?>
+<html>
+<head>
+<title></title>
+<link rel='stylesheet' href='css/style3.css' type='text/css' media='all' />
+</head>
+<body>
+<div style="width:700px; margin:50 auto;">
+
+<a href="index.php">Kembali</a>
+<br />
+<h2>Keranjang</h2>
+
+<?php
+if(!empty($_SESSION["shopping_cart"])) {
+$cart_count = count(array_keys($_SESSION["shopping_cart"]));
+?>
+<div class="cart_div">
+<a href="cart.php">
+<img src="images/cart-icon.png" /> Cart
+<span><?php echo $cart_count; ?></span></a>
+</div>
+<?php
+}
+?>
+
+<div class="cart">
+<?php
+if(isset($_SESSION["shopping_cart"])){
+    $total_price = 0;
+?>
+<table class="table">
+<tbody>
+<tr>
+<td></td>
+<td>Nama Barang</td>
+<td>Jumlah</td>
+<td>Harga Satuan</td>
+<td>Harga Total</td>
+</tr>
+<?php
+foreach ($_SESSION["shopping_cart"] as $product){
+?>
+<tr>
+<td><img src='<?php echo $product["image"]; ?>' width="50" height="40" /></td>
+<td><?php echo $product["name"]; ?><br />
+<form method='post' action=''>
+<input type='hidden' name='code' value="<?php echo $product["code"]; ?>" />
+<input type='hidden' name='action' value="remove" />
+<button type='submit' class='remove'>Hapus Barang</button>
+</form>
+</td>
+<td>
+<form method='post' action=''>
+<input type='hidden' name='code' value="<?php echo $product["code"]; ?>" />
+<input type='hidden' name='action' value="change" />
+<select name='quantity' class='quantity' onChange="this.form.submit()">
+<option <?php if($product["quantity"]==1) echo "selected";?> value="1">1</option>
+<option <?php if($product["quantity"]==2) echo "selected";?> value="2">2</option>
+<option <?php if($product["quantity"]==3) echo "selected";?> value="3">3</option>
+<option <?php if($product["quantity"]==4) echo "selected";?> value="4">4</option>
+<option <?php if($product["quantity"]==5) echo "selected";?> value="5">5</option>
+</select>
+</form>
+</td>
+<td><?php echo "Rp. ".$product["price"]; ?></td>
+<td><?php echo "Rp. ".$product["price"]*$product["quantity"]; ?></td>
+</tr>
+<?php
+$total_price += ($product["price"]*$product["quantity"]);
+}
+?>
+<tr>
+<td colspan="5" align="right">
+<strong>TOTAL: <?php echo "Rp. ".$total_price; ?></strong>
+</td>
+</tr>
+</tbody>
+</table>
+  <?php
+}else{
+	echo "<h3>Keranjang Kosong!</h3>";
+	}
+?>
+</div>
+
+<div style="clear:both;"></div>
+
+<div class="message_box" style="margin:10px 0px;">
+<?php echo $status; ?>
+</div>
+
+</div>
+</body>
+</html>
