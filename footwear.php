05a109b31219204d20421fe2c4ba3469f54482c2
dodotz613 <56112643+dodotz613@users.noreply.github.com>
2021-01-06 14:54:14 +0700

    Update footwear.php

--- footwear.php
+++ footwear.php
@@ -1,3 +1,44 @@
+<?php
+
+session_start();
+include('config.php');
+$status="";
+if (isset($_POST['code']) && $_POST['code']!=""){
+$code = $_POST['code'];
+$result = mysqli_query($con,"SELECT * FROM `product2` WHERE `code`='$code'");
+$row = mysqli_fetch_assoc($result);
+$name = $row['name'];
+$code = $row['code'];
+$price = $row['price'];
+$image = $row['image'];
+
+$cartArray = array(
+	$code=>array(
+	'name'=>$name,
+	'code'=>$code,
+	'price'=>$price,
+	'quantity'=>1,
+	'image'=>$image)
+);
+
+if(empty($_SESSION["shopping_cart"])) {
+	$_SESSION["shopping_cart"] = $cartArray;
+	$status = "<div class='box'>Produk telah ditambahkan ke keranjang!</div>";
+}else{
+	$array_keys = array_keys($_SESSION["shopping_cart"]);
+	if(in_array($code,$array_keys)) {
+		$status = "<div class='box' style='color:red;'>
+		Produk sudah ditambahkan!</div>";
+	} else {
+	$_SESSION["shopping_cart"] = array_merge($_SESSION["shopping_cart"],$cartArray);
+	$status = "<div class='box'>Produk telah ditambahkan ke keranjang!</div>";
+	}
+
+	}
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,151 +57,70 @@
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/aos.css">
     <link rel="stylesheet" href="css/style2.css">
+    <link rel='stylesheet' href='css/style3.css' type='text/css' media='all' />
 
   </head>
   <body>
 
-		<?php
-
-		include('config.php');
-
-
-		$query = "SELECT * FROM produk";
-
-		$result = mysqli_query($conn , $query);
-
-		?>
-
   <div class="site-wrap">
     <div class="bg-light py-3">
       <div class="container">
+        <div class="cart_div">
+        <a href="cart.php"><img src="images/cart-icon.png" /> Cart<span><?php echo $cart_count; ?></span></a>
+        </div>
         <div class="row">
-          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Footwear</strong></div>
+          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Gear</strong></div>
         </div>
       </div>
-    </div>
-
-
-    <div class="site-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-9 order-2">
-            <div class="row mb-5">
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="#"><img src="images/kerir.png" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><?php echo $row['nama_produk']; ?></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold"><?php echo $row['harga']; ?></p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.html"><img src="images/kerir2.jpg" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="shop-single.html">Eiger</a></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold">Rp 150.000</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.html"><img src="images/kerir3.jpg" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-										<h3><a href="shop-single.html">Eiger</a></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold">Rp 150.000</p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.html"><img src="images/kerir4.jpg" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-										<h3><a href="shop-single.html">Eiger</a></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold">Rp 150.000</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.html"><img src="images/kerir5.jpg" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-										<h3><a href="shop-single.html">Eiger</a></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold">Rp 150.000</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.html"><img src="images/kerir6.jpg" alt="Image placeholder" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-										<h3><a href="shop-single.html">Eiger</a></h3>
-                    <p class="mb-0">Mountaineering Rucksack</p>
-                    <p class="text-primary font-weight-bold">Rp 150.000</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-
-
-          <div class="col-md-3 order-1 mb-5 mb-md-0">
-            <div class="border p-4 rounded mb-4">
-              <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
-              <ul class="list-unstyled mb-0">
-                <li class="mb-1"><a href="#" class="d-flex"><span>Men</span></a></li>
-                <li class="mb-1"><a href="#" class="d-flex"><span>Women</span></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <footer class="footerv4-w3ls py-sm-5 py-4">
-    	<div class="footerv4-top">
-    		<div class="container">
-    		</div>
-    	</div>
-    </footer>
-
-    <div class="copy-right text-center">
-    	<p>© 2020 Sulung Darmawan. All rights reserved | Design by
-    		<a href="http://linked.in/sulungdarmawan72"> Sulung Darmawan.</a>
-    	</p>
     </div>
   </div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/main.js"></script>
+
+<?php
+if(!empty($_SESSION["shopping_cart"])) {
+$cart_count = count(array_keys($_SESSION["shopping_cart"]));
+?>
+
+<?php
+}
+
+$result = mysqli_query($con,"SELECT * FROM `product2`");
+while($row = mysqli_fetch_assoc($result)){
+		echo "<div class='product_wrapper'>
+			  	<form method='post' action=''>
+			  	<input type='hidden' name='code' value=".$row['code']." />
+			  	<div class='image'><img src='".$row['image']."' /></div>
+			  	<div class='name'>".$row['name']."</div>
+		   	  <div class='price'>Rp. ".$row['price']."</div>
+			  	<button type='submit' class='buy'>Beli Sekarang</button>
+			  	</form>
+		   	  </div>";
+        }
+mysqli_close($con);
+?>
+
+<div style="clear:both;"></div>
+
+<div class="message_box" style="margin:20px 10px ;">
+<?php echo $status; ?>
+</div>
+
+<footer class="footerv4-w3ls py-sm-5 py-4">
+  <div class="footerv4-top">
+    <div class="container">
+    </div>
+  </div>
+</footer>
 
 
-  </body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/main.js"></script>
+
+</body>
 </html>
