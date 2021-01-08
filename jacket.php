5de8240b7f8687e01fb4520b7790a2b49aac1829
dodotz613 <56112643+dodotz613@users.noreply.github.com>
2021-01-08 14:57:24 +0700

    Update jacket.php

--- jacket.php
+++ jacket.php
@@ -1,190 +1,115 @@
-<?php require_once("auth.php"); ?>
+<?php
+
+session_start();
+include('config.php');
+$status="";
+if (isset($_POST['code']) && $_POST['code']!=""){
+$code = $_POST['code'];
+$result = mysqli_query($con,"SELECT * FROM `product3` WHERE `code`='$code'");
+$row = mysqli_fetch_assoc($result);
+$name = $row['name'];
+$code = $row['code'];
+$price = $row['price'];
+$image = $row['image'];
+
+$cartArray = array(
+	$code=>array(
+	'name'=>$name,
+	'code'=>$code,
+	'price'=>$price,
+	'quantity'=>1,
+	'image'=>$image)
+);
+
+if(empty($_SESSION["shopping_cart"])) {
+	$_SESSION["shopping_cart"] = $cartArray;
+	$status = "<div class='box'>Produk telah ditambahkan ke keranjang!</div>";
+}else{
+	$array_keys = array_keys($_SESSION["shopping_cart"]);
+	if(in_array($code,$array_keys)) {
+		$status = "<div class='box' style='color:red;'>
+		Produk sudah ditambahkan!</div>";
+	} else {
+	$_SESSION["shopping_cart"] = array_merge($_SESSION["shopping_cart"],$cartArray);
+	$status = "<div class='box'>Produk telah ditambahkan ke keranjang!</div>";
+	}
+
+	}
+
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>NORTH THE FACE</title>
-<link rel="shortcut icon" href="images/logo.png" type="image/png">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="utf-8">
-<meta name="keywords" content="Your Adventure Gear in here" />
+  <head>
+    <title>NORTH THE FACE</title>
+    <link rel="shortcut icon" href="images/logo.png" type="image/png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <script>
-        addEventListener("load", function () {
-            setTimeout(hideURLbar, 0);
-        }, false);
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-        function hideURLbar() {
-            window.scrollTo(0, 1);
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel='stylesheet' href='css/style3.css' type='text/css' media='all' />
+
+  </head>
+  <body>
+
+  <div class="site-wrap">
+    <div class="bg-light py-3">
+      <div class="container">
+        <div class="cart_div">
+        <a href="cart.php"><img src="images/cart-icon.png" /> Cart</a>
+        </div>
+        <div class="row">
+          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Gear</strong></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+<?php
+
+
+$result = mysqli_query($con,"SELECT * FROM `product3`");
+while($row = mysqli_fetch_assoc($result)){
+		echo "<div class='product_wrapper'>
+			  	<form method='post' action=''>
+			  	<input type='hidden' name='code' value=".$row['code']." />
+			  	<div class='image'><img src='".$row['image']."' /></div>
+			  	<div class='name'>".$row['name']."</div>
+		   	  <div class='price'>Rp. ".$row['price']."</div>
+			  	<button type='submit' class='buy'>Beli Sekarang</button>
+			  	</form>
+		   	  </div>";
         }
-    </script>
+mysqli_close($con);
+?>
 
-    <script src="https://kit.fontawesome.com/37f7f7be15.js" crossorigin="anonymous"></script>
+<div style="clear:both;"></div>
 
-    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-    <link href="css/style.css" rel='stylesheet' type='text/css' />
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	  <link href="css/css_slider.css" type="text/css" rel="stylesheet" media="all">
-
-
-    <link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900" rel="stylesheet">
-	  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
-
-
-</head>
-<body>
-
-<!-- header -->
-<!-- header -->
-<header>
-	<div class="container">
-		<!-- nav -->
-		<nav class="py-md-4 py-3 d-lg-flex">
-			<label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
-			<input type="checkbox" id="drop" />
-			<ul class="menu mt-lg-3 ml-auto">
-        <li class=""><span><span class="fa fa-user"></span><a href="logout.php"> Logout</a></span></li>
-			</ul>
-		</nav>
-		<!-- //nav -->
-	</div>
-</header>
-<!-- //header -->
-
-<!-- banner -->
-<section class="banner_w3lspvt" id="home">
-	<div class="csslider infinity" id="slider1">
-		<input type="radio" name="slides" checked="checked" id="slides_1" />
-		<ul>
-			<li>
-				<div class="banner-top">
-					<div class="overlay">
-						<div class="container">
-							<div class="w3layouts-banner-info">
-								<h3 class="text-wh"> Your Mountaineering  <span>Gear</span></h3>
-								</div>
-						</div>
-					</div>
-				</div>
-			</li>
-		</ul>
-		<div class="arrows">
-			<label for="slides_1"></label>
-		</div>
-	</div>
-</section>
-<!-- //banner -->
-
-<!--/kategori -->
-    <section class="about py-lg-5 py-md-5 py-3">
-        <div class="container">
-
-                <!-- services -->
-                <div class="fetured-info py-lg-5 py-3">
-                    <h3 class="heading text-center mb-lg-5 mb-3">What we sell</h3>
-                    <div class="row fetured-sec mt-lg-5">
-
-
-                            <div class="featured-center text-center row">
-                                <div class="col-sm-6">
-                                    <div class="bottom-gd fea">
-                                        <span class="fas fa-suitcase" aria-hidden="true"></span>
-                                        <h3 class="my-3">Carrier</h3>
-                                        <p class="px-lg-3">Mountaineering rucksack bag for hiking needed</p>
-                                        <br/>
-                                        <a href="carrier.php" class="btn btn-success btn-md">Shop Now</a>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="bottom-gd fea">
-                                        <span class="fa fa-diagnoses" aria-hidden="true"></span>
-                                        <h3 class="my-3">Moutain Jacket</h3>
-                                        <p class="px-lg-3">Mountain jacket for hiking gear</p>
-                                        <br/>
-                                        <a href="jacket.php" class="btn btn-success btn-md">Shop Now</a>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="bottom-gd fea">
-                                        <span class="fa fa-gear" aria-hidden="true"></span>
-                                        <h3 class="my-3">Mountain Gear</h3>
-                                        <p class="px-lg-3">Mountaineering gear for hiking needed</p>
-                                        <br/>
-                                        <a href="gear.php" class="btn btn-success btn-md">Shop Now</a>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="bottom-gd fea">
-                                        <span class="fas fa-shoe-prints" aria-hidden="true"></span>
-                                        <h3 class="my-3">Footwear</h3>
-                                        <p class="px-lg-3">Mountain boots for hiking gear</p>
-                                        <br/>
-                                        <a href="footwear.php" class="btn btn-success btn-md">Shop Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- //services -->
-    </section>
-  <!-- //kategori -->
-
-
-<!-- footer -->
-<footer class="footerv4-w3ls py-sm-5 py-4">
-	<div class="footerv4-top">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 footer-left">
-					<h3>Hubungi Kami</h3>
-				</div>
-				<!-- footer nav links -->
-				<div class="col-md-8 footer-nav-wthree my-md-3 mt-3">
-					<ul class="social-icons list-unstyled">
-						<li>
-							<a href="#">
-								<span class="fa fa-facebook"></span>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="fa fa-twitter"></span>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="fa fa-google-plus"></span>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span class="fa fa-instagram"></span>
-							</a>
-						</li>
-					</ul>
-				</div>
-				<!-- //footer nav links -->
-
-			</div>
-		</div>
-	</div>
-</footer>
-<!-- //footer -->
-<!-- copyright -->
-<div class="copy-right text-center">
-	<p>© 2020 Sulung Darmawan. All rights reserved | Design by
-		<a href="http://linked.in/sulungdarmawan72"> Sulung Darmawan.</a>
-	</p>
+<div class="message_box" style="margin: auto;">
+<?php echo $status; ?>
 </div>
-<!-- //copyright -->
 
-
-
-<!-- move top icon -->
-<a href="#home" class="move-top text-center"></a>
-<!-- //move top icon -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/main.js"></script>
 
 </body>
 </html>
